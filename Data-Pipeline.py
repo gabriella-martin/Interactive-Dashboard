568be88ceddc9309a9f3ecc1f68d6be4919461e4
Gabriella Martin <gabriella.martin0101@gmail.com>
2023-01-18 09:30:23 +0000

    initalise health pipeline

--- Data-Pipeline.py
+++ Data-Pipeline.py
@@ -5,12 +5,18 @@ import requests
 import shutil
 
 from datetime import datetime, timedelta
+from decouple import config
 from pyicloud import PyiCloudService
 from shutil import copyfileobj
 
 class HealthPipeline:
 
+     def __init__(self):
+        self.yesterdays_date_only = (str(datetime.today() - timedelta(1)))[:10]
+        self.api = PyiCloudService(config('APPLE_ID'), config('APPLE_ID_PASSWORD'))
+        self.yesterdays_data_in_list = []
+
+        
 class WorkPipeline:
 
 class PersonalPipeline:
-    
