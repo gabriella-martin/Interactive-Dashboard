e09b2cf89de7ae2a73842f90db3d7c0b98c8578f
Gabriella Martin <gabriella.martin0101@gmail.com>
2023-01-27 11:15:27 +0000

    migrate health data to another file

--- Data-Pipeline.py
+++ Data-Pipeline.py
@@ -15,57 +15,6 @@ from shutil import copyfileobj
 from todoist_api_python.api import TodoistAPI
 
 
-class AppleHealthPipeline:
-
-    def __init__(self):
-        self.todays_date_only = (str(datetime.today() - timedelta(1)))[:10]
-        self.api = PyiCloudService(config('APPLE_ID'), config('APPLE_ID_PASSWORD'))
-        self.yesterdays_data_in_list = []
-
-    def connect_to_api(self):
-        if self.api.requires_2fa:
-            print("Two-factor authentication required.")
-            code = input("Enter the code you received of one of your approved devices: ")
-            result = self.api.validate_2fa_code(code)
-            print("Code validation result: %s" % result)
-
-            if not result:
-                print("Failed to verify security code")
-                sys.exit(1)
-
-            if not self.api.is_trusted_session:
-                print("Session is not trusted. Requesting trust...")
-                result = self.api.trust_session()
-                print("Session trust result %s" % result)
-
-                if not result:
-                    print("Failed to request trust. You will likely be prompted for the code again in the coming weeks")
-            
-        self.yesterdays_health_data_document = self.api.drive[f'HealthAutoExport-{self.todays_date_only}-{self.todays_date_only} Data.csv']
-
-        return(self.yesterdays_health_data_document)      
-
-    def extract_csv_data_to_python_list(self):
-
-        with self.yesterdays_health_data_document.open(stream=True) as response:
-            with open(self.yesterdays_health_data_document.name, 'wb') as file_out:
-                copyfileobj(response.raw, file_out)
-        with open(f'HealthAutoExport-{self.todays_date_only}-{self.todays_date_only} Data.csv') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in csv_reader:
-                if row[0] == 'Date':
-                    continue
-                else:
-                    self.yesterdays_data_in_list.append(row[1])
-                    self.yesterdays_data_in_list.append(row[2])
-                    self.yesterdays_data_in_list.append(row[3])
-                    self.yesterdays_data_in_list.append(row[5])
-        return(self.yesterdays_data_in_list)
-
-    def get_health_data(self):
-        self.connect_to_api()
-        self.extract_csv_data_to_python_list()
-
 class TrackingTimePipeline:
 
     def __init__(self):
@@ -229,28 +178,6 @@ class TodoistPipeline:
         collated_lists = self.collate_categories()
         self.find_percentages_of_completed(collated_lists)
 
-class WithingsPipeline:
-
-    def connect_to_api(self):
-        self.body_measurements = []
-        headers =  {"Authorization": f"Bearer {config('WITHINGS_ACCESS_TOKEN')}"}
-        data = {'action': 'getmeas','meastypes': '1,6' }
-        response = requests.post('https://wbsapi.withings.net/measure', headers=headers, data=data)
-        response = response.json()
-        return response
-
-    def parse_data(self, response):
-        data_items = response['body']['measuregrps'][0]['measures']
-        for data_item in data_items:
-            data_value = str((data_item['value']))[:2] + '.' + str((data_item['value']))[2:4]
-            data_value = float(data_value)
-            self.body_measurements.append(data_value)
-
-        return(self.body_measurements)
-        
-    def withings_pipeline(self):
-        response = self.get_data()
-        self.parse_data(response)
 
 class GoogleCalendarPipeline:
 
@@ -355,7 +282,7 @@ class ExistPipeline():
         sleep_minutes = round(((int((str(sleep_time/60))[2:4]))/100)*60)
         sleep_string = sleep_hours + 'hr' + str(sleep_minutes)
     
-        return(sleep_string, wake_string)
+        return(sleep_string, wake_string, sleep_time)
 
 
 class AirTablePipeline():
