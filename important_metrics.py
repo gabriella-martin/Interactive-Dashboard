8d1804959abbfddf1a7fecb425316d42cf02e3bd
Gabriella Martin <gabriella.martin0101@gmail.com>
2023-01-31 09:08:53 +0000

    refactor code

--- important_metrics.py
+++ important_metrics.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 df = pd.read_csv('Database.csv')
 number_of_entries = len(df)
 
@@ -6,59 +7,35 @@ class ImportantMetrics:
 
     def __init__(self, metric_list):
         self.metric_list = metric_list
-        self.yesterdays_metrics = []
-        self.yesterday_vs_day_before_yesterday_percent_change = []
-        self.three_day_averages = []
-        self.current_three_day_vs_past_three_day = [] 
-        self.seven_day_averages = []
-        self.current_seven_day_vs_past_seven_day = []
 
-    def get_yesterdays_metrics(self):
+        
+        time_period_percent_change = []
+
+    def get_time_period_metric(self, date_range=int):
+        time_period_averages = []
         for i in self.metric_list:
-            yesterday_metric = df.iloc[number_of_entries -1][i]
-            self.yesterdays_metrics.append(yesterday_metric)
-        return self.yesterdays_metrics
+            time_period_metric = 0
+            for a in range(1,date_range+1):
+                time_period_metric += df.iloc[number_of_entries-a][i]
+            time_period_averages.append(round(time_period_metric/date_range))
+        return time_period_averages
 
-    def get_yesterday_percentage_change(self):
-        self.yesterdays_metrics = self.get_yesterdays_metrics()
-        for index, value in enumerate(self.yesterdays_metrics):
-            percent_change = round((((value - (df.iloc[number_of_entries-2])[self.metric_list[index]]))/((df.iloc[number_of_entries-2])[metric_list[index]]))*100)
-            self.yesterday_vs_day_before_yesterday_percent_change.append(percent_change)
-        return self.yesterday_vs_day_before_yesterday_percent_change
-    
-    def get_three_day_averages(self):
-        for category in self.metric_list:
-            three_day_sum = 0
-            for i in range(1,4):
-                metric = df.iloc[number_of_entries - i][category]
-                three_day_sum = three_day_sum + metric
-            three_day_average =round((three_day_sum)/3)
-            self.three_day_averages.append(three_day_average)
-        return self.three_day_averages
+    def get_time_period_percent_change(self,date_range=int):
+        time_period_averages = self.get_time_period_metric(date_range)
+        time_period_before_averages = []
+        time_period_percent_change = []
+        for i in self.metric_list:
+            time_period_before_metric = 0
+            for a in range(1 +date_range,(date_range*2)+1):
+                time_period_before_metric += df.iloc[number_of_entries-a][i]
+            time_period_before_averages.append(round(time_period_before_metric/date_range))
 
-    def get_three_day_percentage_change(self):
-        self.three_day_averages = self.get_three_day_averages 
-        for index, value in enumerate(self.three_day_averages):
-            three_day_earlier_average = ((((df.iloc[number_of_entries-4])[self.metric_list[index]]) + (df.iloc[number_of_entries-5][self.metric_list[index]]) + (df.iloc[number_of_entries-6])[self.metric_list[index]]))/3
-            percent_change = round((((value - three_day_earlier_average))/three_day_earlier_average)*100)
-            self.current_three_day_vs_past_three_day.append(percent_change)
-        return self.current_three_day_vs_past_three_day
+        for index, value in enumerate(time_period_averages):
+            percent_change = round(((value - time_period_before_averages[index])/time_period_before_averages[index])*100)
+            time_period_percent_change.append(percent_change)
 
-    def get_seven_day_averages(self):
-        for category in self.metric_list:
-            seven_day_sum = 0
-            for i in range(1,8):
-                metric = df.iloc[number_of_entries - i][category]
-                seven_day_sum = seven_day_sum + metric
-            seven_day_average =round((seven_day_sum)/7)
-            self.seven_day_averages.append(seven_day_average)
-        return self.seven_day_averages
+        return time_period_percent_change
 
-    def get_seven_day_percentage_change(self):
-        self.seven_day_averages = self.get_seven_day_averages()
-        for index, value in enumerate(self.seven_day_averages):
-            seven_day_earlier_average = ((((df.iloc[number_of_entries-8])[self.metric_list[index]]) + (df.iloc[number_of_entries-9][self.metric_list[index]]) + (df.iloc[number_of_entries-10])[self.metric_list[index]]) + 
-        (df.iloc[number_of_entries-11][self.metric_list[index]]) + (df.iloc[number_of_entries-12][self.metric_list[index]]) + (df.iloc[number_of_entries-13][self.metric_list[index]]) + (df.iloc[number_of_entries-14][self.metric_list[index]]) )/7
-            percent_change = round((((value - seven_day_earlier_average))/seven_day_earlier_average)*100)
-            self.current_seven_day_vs_past_seven_day.append(percent_change)
-        return self.current_seven_day_vs_past_seven_day
+
+
+
